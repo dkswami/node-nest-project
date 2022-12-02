19383fe808bc2c77b62640ccc41fa22b02996696
dkswami <dkswami1820@gmail.com>
2022-12-02 13:22:51 +0530

    added items module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemsController } from './items/items.controller';
 import { ItemsService } from './items/items.service';
+import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, ItemsController],
-  providers: [AppService, ItemsService],
+  imports: [ItemsModule,],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
